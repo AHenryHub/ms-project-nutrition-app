b9ac8cad6cc20e42a147511a84d4a04c90ccadc4
unknown <constanceabernathy1900@gmail.com>
2023-07-22 17:43:31 -0400

    fixing query file, Alex Elijah

--- query_db.py
+++ query_db.py
@@ -1,13 +1,17 @@
 import json
 
+
 def query_db(query):
     f = open('db.json')
 
-    data = json.load(f)
+    data = json.loads(f)
 
     for i in data:
-        if i["recipeid"]==query:
+        if json.loads(i)["recipeid"] == query:
             f.close()
             return i
 
     f.close()
+
+
+query_db(0)
