19b8d086a2bcba44c1a18b0b408f48948dd964e4
unknown <constanceabernathy1900@gmail.com>
2023-07-22 17:20:47 -0400

    Elijah, Alex, first query_db

--- query_db.py
+++ query_db.py
@@ -1,32 +1,13 @@
-from flask import Flask,jsonify
-from pysondb import db
+import json
 
-"""
-Adding to db
+def query_db(query):
+    f = open('db.json')
 
-a=db.getDb("pathtojson.json")
->> a.add({"name":"pysondb","type":"DB"})
->> # returns 1929323232 which is a ID assigned to the above data.
->> a.add({"namme":"pyson","type":"DB"})
-"""
+    data = json.load(f)
 
-"""
-Query db
+    for i in data:
+        if i["recipeid"]==query:
+            f.close()
+            return i
 
-a=db.getDb("path.json")
->> a.getBy({"type":"CLI"})
->> [{"name":"py_cli","type":"CLI"},{"name":"py_cli2","type":"CLI"}]
->> a.getBy({"name":"py_cli"})
->> [{"name":"py_cli","type":"CLI"}]
-"""
-
-
-def query():
-    base = db.getDb("db.json")
-    base.add({"name":"recipebook","type":"DB"})
-
-    print(base.getBy({"type":"DB"}))
-
-    return base
-
-query()
+    f.close()
